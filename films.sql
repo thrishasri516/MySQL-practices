5d992d2917d91a468ff1b828f8734852e0090304
Thrisha sri <thrishasri5112004@gmail.com>
2026-05-18 15:22:55 +0530

    Add files via upload

---
+++ films.sql
@@ -0,0 +1,7 @@
+use sakila;
+show tables;
+select * from customer;
+select * from address;
+select * from store;
+select * from film;
+select * from film where special_features like '%deleted scenes%';
