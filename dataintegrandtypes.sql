2b5fa2615534bbc0a87018d48cf82b511e8d3490
Thrisha sri <thrishasri5112004@gmail.com>
2026-05-18 15:20:20 +0530

    Add files via upload

---
+++ dataintegrandtypes.sql
@@ -0,0 +1,14 @@
+data integrity
+     data types mobile numeric(10)
+     constraints length(mobile)=10
+     programmed validations
+     
+     
+numeric/decimal maths marks numeric(3)
+int - tiny,small,medium,big		 tiny int-0 to 255/small int-0 to 65000
+date
+time
+datetime
+char             stuname char(10) 'raja      ' 1 byte space#fixed length
+varchar          stuname varchar(10)'raja'   
+varchar2         stuname varchar2(10) international lang  16 byte space
